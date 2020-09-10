89e2ffa150badca53d0ef5cc30aef0293619abc4
Manuela Carvalho <manuela_carvalho_6@hotmail.com>
2020-09-10 17:40:21 -0300

    Classe Elevador

---
+++ Elevador_Classe.java
@@ -0,0 +1,70 @@
+package Exe_2P;
+
+public class Elevador_Classe {
+
+	private int idElevador;
+	private boolean ligado;
+	private boolean prioridade;
+	private int andarAtual;
+	
+	public Elevador_Classe() {
+		
+	}
+	
+	public Elevador_Classe(int idElevador, boolean ligado, boolean prioridade, int andarAtual) {
+		this.idElevador = idElevador;
+		this.ligado = ligado;
+		this.prioridade = prioridade;
+		this.andarAtual = andarAtual;
+	}
+
+	public int getIdElevador() {
+		return idElevador;
+	}
+
+	public void setIdElevador(int idElevador) {
+		this.idElevador = idElevador;
+	}
+
+	public boolean isLigado() {
+		return ligado;
+	}
+
+	public boolean isPrioridade() {
+		return prioridade;
+	}
+
+	public int getAndarAtual() {
+		return andarAtual;
+	}
+
+	public void setAndarAtual(int andarAtual) {
+		this.andarAtual = andarAtual;
+	}	
+
+	public void ligarDesligar() {
+		ligado = !(ligado);
+	}
+	
+	public void ativarPrioridade() {
+		prioridade = true;
+	}
+
+	public void desativarPrioridade() {
+		prioridade = false;
+	}
+	
+	public float calcularEsforco(int andarDestino) {
+		float esforco = 0;
+		if (andarDestino > andarAtual) {
+			esforco = 1.25f * (andarDestino - andarAtual);
+		} else  {
+			esforco = andarDestino - andarAtual;
+		}
+		return esforco;
+	}
+
+	
+	
+	
+}
